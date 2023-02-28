1877fdbae609a46e2264b2bbf7257dbab450b833
Aidan-TC <tuckerai@oregonstate.edu>
2023-02-27 17:26:11 -0800

    update for assignment 10

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/translate', async (req, res) => {
     let text = req.query.text;
